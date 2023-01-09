23fe2a724b2177968f9dacaadda77f894306383e
ouhna-elhassan <ouhnahassan01@gmail.com>
2023-01-09 16:32:22 +0100

    save changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,8 @@
-//import 'package:deliveryli/test/screens/Authentication/login.dart';
-//import 'package:deliveryli/test/screens/Authentication/verification.dart';
-//import 'package:deliveryli/test/screens/home.dart';
-import 'package:deliveryli/app/screens/home.dart';
 import 'package:deliveryli/app/screens/sliders/splash.dart';
 import 'package:deliveryli/app/services/mapState.dart';
 import 'package:flutter/material.dart';
-//import 'test/components/splash.dart';
-//import 'test/components/sliders.dart';
-import 'app/navigationBar.dart';
-//import 'test/screens/mapScreen.dart';
-
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
-//import 'test/screens/forme.dart';
-//import 'test/screens/Authentication/verification.dart';
 import 'package:provider/provider.dart';
 import './app/services/chatServices/chatNotifier.dart';
 
